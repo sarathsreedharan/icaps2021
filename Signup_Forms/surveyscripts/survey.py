ede503282dbeb53adea440c9aeb4959463973d63
Sarath Sreedharan <sarathsreedharan@Saraths-MacBook-Pro.local>
2021-07-24 10:46:24 -0700

    Adding signup forms

---
+++ Signup_Forms/surveyscripts/survey.py
@@ -0,0 +1,38 @@
+import requests
+from flask import Flask, render_template, request, session
+import sys
+import os
+import copy
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_path)
+from survey_creds import * 
+
+import time
+import json
+app = Flask(__name__)
+
+from firebase import Firebase
+firebase = Firebase(FIREBASE_CONFIG)
+db = firebase.database()
+CHECKBOX = ["aug_9_8","aug_9_9","aug_9_16","aug_9_17","aug_9_22","aug_9_23","aug_10_8","aug_10_9","aug_10_16","aug_10_17","aug_10_22","aug_10_23","aug_11_8","aug_11_9","aug_11_17","aug_11_22","aug_11_23","aug_12_8","aug_12_9","aug_12_16","aug_12_17","aug_12_22","aug_12_23","aug_13_8","aug_13_9","aug_13_16","aug_13_17","aug_13_22","aug_13_23"]
+
+@app.route("/chat", methods=['POST'])
+def index():
+    data_dict = {}
+    data_dict["name"] = request.form.get("name")
+    data_dict["email"] = request.form.get("email")
+    data_dict["status"] = request.form.get("status")
+    data_dict["other_researchers"] = request.form.get("other_researchers")
+    data_dict["table"] = request.form.get("table")
+
+   
+    for dt in CHECKBOX:
+        data_dict[dt] = request.form.get(dt,0)
+ 
+    db.child("CHAT").push(data_dict)
+    return render_template('submit.html')
+ 
+if __name__ == "__main__":
+    app.run()
---
+++ Signup_Forms/surveyscripts/survey_creds.py
@@ -0,0 +1,8 @@
+GOOGLE_API_KEY="AIzaSyDRSbNzOJ0WD0l7e6vvElMJnDPto1A1Mew"
+
+FIREBASE_CONFIG = {'apiKey': "AIzaSyDQjM8QBEemAxjHsIlfLAHfBf_y5bsCDqk", 'authDomain': "icaps-survey.firebaseapp.com", 'databaseURL': "https'://icaps-survey-default-rtdb.firebaseio.com", 'projectId': "icaps-survey", 'storageBucket': "icaps-survey.appspot.com", 'messagingSenderId': "498087478865", 'appId': "1':498087478865':web':52147e91b67771e4f41a70", 'measurementId': "G-H65KZ44FB0", "databaseURL": "https://icaps-survey-default-rtdb.firebaseio.com/"}
+
+
+#'apiKey': "AIzaSyAkaaNon_2l6JEjaE_z3w_FrFQIojtipXQ", 'authDomain': "icaps-demos.firebaseapp.com", 'projectId': "icaps-demos", 'storageBucket': "icaps-demos.appspot.com", 'messagingSenderId': "1097757618013", 'appId': "1:1097757618013:web:fc96f9c067c518ce6f42b4", 'measurementId': "G-3105V6HDLT", "databaseURL": "https://icaps-demos-default-rtdb.firebaseio.com/"}
+
+
