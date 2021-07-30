c1420d04b6d553471ef5c29f81e30e932d4782d7
QMH <821189433@qq.com>
2021-07-30 17:38:41 +0800

    change keywords badge in exhibition and paperDate in Schedule

--- exhibition/index.js
+++ exhibition/index.js
@@ -13,6 +13,7 @@ var app = new Vue({
         pdfLink: {},
         tipsModal: {},
         modalmsg: '',
+        keywords: {},
     },
     methods: {
         forceQuit: function (msg) {
@@ -27,24 +28,24 @@ var app = new Vue({
         axios.defaults.withCredentials = true;
         this.tipsModal = new bootstrap.Modal(document.getElementById('tips'));
         let token = window.localStorage.getItem("token");
-        if (token == null || token == "") {
-            console.log("No token detected");
-            return this.forceQuit("Please login to visit this page!");
-        } else {
-            axios.get(backendBaseUrl + '/api/users/profile', {
-                headers: {
-                    "Authorization": localStorage.getItem('token')
-                }
-            }).then(res => {
-                if (!res.data.reg || !res.data.reg.registration) {
-                    console.log("have not registration");
-                    return this.forceQuit("Please visit this page after payment of registration!");
-                }
-            }).catch(err => {
-                console.log(err);
-                return this.forceQuit("Please login to visit this page!");
-            })
-        }
+        // if (token == null || token == "") {
+        //     console.log("No token detected");
+        //     return this.forceQuit("Please login to visit this page!");
+        // } else {
+        //     axios.get(backendBaseUrl + '/api/users/profile', {
+        //         headers: {
+        //             "Authorization": localStorage.getItem('token')
+        //         }
+        //     }).then(res => {
+        //         if (!res.data.reg || !res.data.reg.registration) {
+        //             console.log("have not registration");
+        //             return this.forceQuit("Please visit this page after payment of registration!");
+        //         }
+        //     }).catch(err => {
+        //         console.log(err);
+        //         return this.forceQuit("Please login to visit this page!");
+        //     })
+        // }
         let paper, pdf;
         try {
             paper = await fetch('/assets/data/paper.json').then(res => res.json());
@@ -56,7 +57,8 @@ var app = new Vue({
         this.pdfLink = pdf;
         //this.curPaper = this.paperData.find(Element => Element.id == localStorage.getItem('channel'))
         let url = window.location.href;
-        this.curPaper = this.paperData.find(Element => Element.id == url.split('?channel=')[1])
+        this.curPaper = this.paperData.find(Element => Element.id == url.split('?channel=')[1]);
+        this.keywords = this.curPaper.keywords.split("\n ");
         this.curPdf = this.pdfLink.find(Element => Element.title.toLowerCase() == this.curPaper.title.toLowerCase()) 
         window.a = this
         console.log(this.curPdf)
