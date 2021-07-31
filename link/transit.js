db8a182eed37d1f4200d942aa480823c22f48a70
wx_cf3348ab12 <1934410205@qq.com>
2021-07-31 17:16:10 +0800

    add some sponsors

--- link/transit.js
+++ link/transit.js
@@ -8,15 +8,20 @@ var app = new Vue({
         posters:['http://'+url+'/link/posters/index.html?id=',''],
         workshops:['http://'+url+'/link/workshops/',''],
         slideslive:['http://'+url+'/exhibition?channel=','']
-    }
+    },
+      sponsor:{
+        ibm:'https://www.ibm.com',
+        artificial_intelligence:'https://www.journals.elsevier.com/artificial-intelligence',
+        huawei:'https://www.noahlab.com.hk/#/about',
+      }
   },
   mounted(){
       let query = location.search;
       let searchParams = new URLSearchParams(query);
       var type = searchParams.get('type');
       var id = searchParams.get('id');
-      if(id == 'huawei'){
-        this.goal_url = 'https://www.noahlab.com.hk/#/about'
+      if(id == 'huawei' || id == 'ibm' || id=='artificial_intelligence'){
+        this.goal_url = this.sponsor[id]
       }else{
         this.goal_url =  this.select[type][0]+id+this.select[type][1]
       }
