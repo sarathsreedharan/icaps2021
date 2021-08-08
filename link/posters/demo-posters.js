7cd5ad81cfff07c8ae8dcdaab0b1cbfb9751bfd8
xiaoyingkai <1934410205@qq.com>
2021-08-08 22:30:46 +0800

    add demo pdf

--- link/posters/demo-posters.js
+++ link/posters/demo-posters.js
@@ -3,5 +3,5 @@ window.onload = function (){
     let query = location.search;
     let searchParams = new URLSearchParams(query);
     var id = searchParams.get('id');
-    document.getElementById('pdf').src="./assets/HPlan-posters/"+id+".pdf";
+    document.getElementById('pdf').src="./assets/Demo-posters/"+id+"-poster.pdf";
 };
