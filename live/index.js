940c0e9cdda211eff37dc451ddc1ac575277c63c
root <root@ip-172-31-22-42.us-east-2.compute.internal>
2021-08-12 16:25:16 +0000

    change the live

--- live/index.js
+++ live/index.js
@@ -44,7 +44,7 @@ var app = new Vue({
             })
         }
 
-        this.slideId = 38965455;
+        this.slideId = 38965498;
         let embed = new SlidesLiveEmbed('presentation-embed', {
             presentationId: this.slideId,
             autoPlay: false, // change to true to autoplay the embedded presentation
